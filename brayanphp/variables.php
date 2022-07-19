def314a5d9fed97d8a1dc02d684cd278b7149a0f
dannyardila <dannyardila059@gmail.com>
2022-07-19 10:18:12 -0500

    Desarrollo: de los ejercicios en php

---
+++ brayanphp/variables.php
@@ -0,0 +1,15 @@
+<?php
+ //Esto es un comentario..
+
+ //mi primera impresion
+ echo "Hola Brayan";
+ echo "<br/>";
+ 
+ $nombre = "Brayan";
+ $apellido = "Fandiño";
+ $edad = "19";
+
+ //concatenacion
+ $imprimir = $nombre . " ". $apellido . " ". $edad;
+ echo $imprimir;
+?>
